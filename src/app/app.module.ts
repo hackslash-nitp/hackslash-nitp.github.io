e844053b2cbfb8c8c34814c4bb3b5bfdc8782c11
DIVYA PRAKASH <medivyaprakash@gmail.com>
2020-02-02 14:33:50 +0530

    initiate

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EventsComponent } from './events/events.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EventsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
