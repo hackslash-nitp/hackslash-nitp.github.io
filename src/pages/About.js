7aaa3d003a40810dfb792b9d90dbfef897d5d89e
PARTH <parthcricketer07@gmail.com>
2020-04-01 21:30:39 +0530

    JS and SCSS pages almost complete

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,68 @@
 import React from 'react';
+import ReactDOM from "react-dom";
+
 
 const About = () => {
-    return(
-        <div className="events">
-        
+    return (
+        <div style="width:99%">
+<body>
+  <div>
+    <div class="row d-flex about-main-div">
+      <div class="col-md-6 col-xl-8 d-flex flex-column justify-content-center">
+        <div class="row d-flex justify-content-center">
+          <div class="col-xl-12 offset-xl-1 d-flex about-div-sub">
+            <h1 class="d-flex flex-grow-1 flex-shrink-1 align-items-center align-content-center align-self-center about-header-first">
+              About Us
+            </h1>
+          </div>
         </div>
-    )
-}
+        <div class="row">
+          <div class="col-xl-11">
+            <h6 class="d-inline-flex align-content-center align-self-center img-fluid about-header-second">
+              HackSlash, currently under the Mozilla Campus Clubs Network, is an all student driven developers community&nbsp;located in the National Institute of Technology Patna. The club strives to build and improve the open sourcing&nbsp;environment
+              and the coding culture of the whole city of Patna and eventually the whole state of Bihar. The will&nbsp;power and determination of each Developers, Competitive Coders, Designers and Innovative thinkers is what&nbsp;governs
+              our great community.<br/>
+            </h6>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 col-xl-4 offset-xl-1 about-div-second"><img class="visible img-fluid about-img" src="assets/img/about_us/about-bg-1.svg"/></div>
+    </div>
+  </div>
+  <div>
+    <div class="row d-flex about-div-third" id="story">
+      <div class="col-md-4 col-lg-4 col-xl-4 offset-xl-1 about-div-second"><img class="visible img-fluid about-img" src="assets/img/about_us/about-bg-2.svg"/></div>
+      <div class="col-md-6 col-xl-8 d-flex flex-column justify-content-center">
+        <div class="row d-flex justify-content-center">
+          <div class="col-xl-12 offset-xl-1 d-flex about-div-sub">
+            <h1 class="d-flex flex-grow-1 flex-shrink-1 align-items-center align-content-center align-self-center about-header-first">
+              Our Story
+            </h1>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xl-11">
+            <h6 class="d-inline-flex align-content-center align-self-center img-fluid about-header-second">
+              Formed as a college’s development club in NIT Patna, it was established on 2018, with only 10 members. The main reason of taking such a step, i.e. to form the HackSlash Club, was because of the lack of awareness and opportunities
+              for the students to achieve their maximum potential and also to bring their attention to the current need in the field of development and open sourcing contribution. It started out as a weekly meet up in&nbsp;the cafeteria
+              to fully functioning weekend classes and afternoon informative sessions. Currently It is holding&nbsp;over<br/>15 core members and 112 interested members of all years and degree programs in the institute itself. We are&nbsp;currently
+              striving of increasing our reach to all other interested students in other technical institutes in Patna.<br/>
+            </h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</body>
+<div class="row d-flex justify-content-center align-content-cente about-bottom-divr" id="team">
+  <div class="col d-flex justify-content-center align-content-center">
+    <h1 class="about-header-first" id="t">Our Team</h1>
+  </div>
+                                </div>
+                            </div>
+
+        );
+} ;
 
 export default About;
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,11 +1,79 @@
 import React from 'react';
+import ReactDOM from "react-dom";
+
 
 const Events = () => {
-    return(
-        <div className="events">
+    var item = {location: "Patna", name: "Test", detail: "Detailed", time: "12PM"};
+    return (
+        <body class="events-body">
+            <img class="float-left visible img-fluid" data-toggle="tooltip" data-bs-tooltip="" src="assets/img/events/events-bg-1.svg"/>
+                <div>
+                    <div class="row events-div">
+                        <div class="col-md-6 col-xl-7 d-flex flex-column justify-content-center align-items-center">
+                            <div class="row d-flex justify-content-center">
+                                <div class="col-xl-12 offset-xl-1 d-flex">
+                                    <h1 class="d-flex flex-grow-1 flex-shrink-1 justify-content-center align-items-center align-content-center align-self-center events-header">Ready to Join the Movement?</h1>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xl-11">
+                                    <h4 id="in" class="d-inline-flex align-content-center align-self-center img-fluid events-header-fourth">Check out What's Happening in the Campus</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-xl-4 offset-xl-1">
+                            <img class="visible img-fluid events-img" src="assets/img/events/events-bg-2.svg"/>
+                                </div>
+                        </div>
+                        <div class="row" style="margin-right: 0px;margin-left: 0px;"><div class="col">
+                            <div class="row">
+                                <div class="col-2 d-flex justify-content-end"><img class="img-fluid" src="assets/img/events/events-ic-search.svg"/>
+                                    </div>
+                                    <div class="col-xl-4 col-md-4 col-sm-5"><input class="form-control-plaintext events-input" type="text" placeholder="Search"/></div>
+                                    </div>
+      </div>
+    </div>
+  </div>
+  <div>
+    <div class="row events-div-second">
+      <div class="col-12 col-md-4 col-xl-2 d-flex" id="upcom">
+        <h5 class="m-auto events-header-fifth">Upcoming</h5>
+        <h5 class="m-auto events-header-fifth">Past</h5>
+      </div>
+      <div class="col">
+        <hr/>
+      </div>
+    </div>
+    <div class="row events-div-third">
+      <img id="bg-4" class="img-fluid events-img-second" src="assets/img/events/events-bg-4.svg" />
+      <img id="bg-3" class="img-fluid events-img-third" src="assets/img/events/events-bg-3.svg" />
+
+      <div class="card border rounded shadow col-sm-6 col-lg-3 item events-div-card">
+        <img class="card-img-top w-100 d-block"/>
+        <div class="location events-location">
+          <img src="assets/img/events/event-ic-location.svg" width="12px" height="12px"
+               style="margin-right: 4px;margin-top: -2px;z-index:5;"/>
+          item.location
+        </div>
+        <img class="img-fluid events-img-fluid" src="https://images.unsplash.com/photo-1531058020387-3be344556be6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" />
+
+        <div class="card-body">
+          <h4 class="card-title events-header-fifth">item.name</h4>
+          <p class="card-text events-header-fifth">
+            item.detail
+          </p>
+
+          <div class="location">
+            <img src="assets/img/events/event-ic-time.svg" width="12px" height="12px"
+                 class="events-img-location"/>
+            item.time
+          </div>
 
         </div>
-    )
-}
-
-export default Events;
+      </div>
+    </div>
+  </div>
+</body>
+);
+};
+        export default Events;
