aa118b699f32e6e0a10ca770f7da6a005e8fb056
PARTH <parthcricketer07@gmail.com>
2020-04-05 23:20:27 +0530

    events page almost complete

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,80 +1,161 @@
 import React from 'react';
 import ReactDOM from "react-dom";
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../assets/scss/Events.scss';
+import topLeft from '../assets/images/events/topLeft.svg';
+import search from '../assets/images/searchIcon.jpg';
+import topRight from '../assets/images/events/topRight.svg';
+import date from '../assets/images/events/date.svg';
+import time from '../assets/images/events/time.svg';
 
 
 const Events = () => {
     var item = {location: "Patna", name: "Test", detail: "Detailed", time: "12PM"};
     return (
-        <body class="events-body">
-            <img class="float-left visible img-fluid" data-toggle="tooltip" data-bs-tooltip="" src="assets/img/events/events-bg-1.svg"/>
-                <div>
-                    <div class="row events-div">
-                        <div class="col-md-6 col-xl-7 d-flex flex-column justify-content-center align-items-center">
-                            <div class="row d-flex justify-content-center">
-                                <div class="col-xl-12 offset-xl-1 d-flex">
-                                    <h1 class="d-flex flex-grow-1 flex-shrink-1 justify-content-center align-items-center align-content-center align-self-center events-header">Ready to Join the Movement?</h1>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-xl-11">
-                                    <h4 id="in" class="d-inline-flex align-content-center align-self-center img-fluid events-header-fourth">Check out What's Happening in the Campus</h4>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-xl-4 offset-xl-1">
-                            <img class="visible img-fluid events-img" src="assets/img/events/events-bg-2.svg"/>
-                                </div>
-                        </div>
-                        <div class="row events-div">
-                          <div class="col">
-                            <div class="row">
-                                <div class="col-2 d-flex justify-content-end"><img class="img-fluid" src="assets/img/events/events-ic-search.svg"/>
-                                    </div>
-                                    <div class="col-xl-4 col-md-4 col-sm-5"><input class="form-control-plaintext events-input" type="text" placeholder="Search"/></div>
-                                    </div>
-      </div>
-    </div>
-  </div>
-  <div>
-    <div class="row events-div-second">
-      <div class="col-12 col-md-4 col-xl-2 d-flex" id="upcom">
-        <h5 class="m-auto events-header-fifth">Upcoming</h5>
-        <h5 class="m-auto events-header-fifth">Past</h5>
-      </div>
-      <div class="col">
-        <hr/>
-      </div>
-    </div>
-    <div class="row events-div-third">
-      <img id="bg-4" class="img-fluid events-img-second" src="assets/img/events/events-bg-4.svg" />
-      <img id="bg-3" class="img-fluid events-img-third" src="assets/img/events/events-bg-3.svg" />
-
-      <div class="card border rounded shadow col-sm-6 col-lg-3 item events-div-card">
-        <img class="card-img-top w-100 d-block"/>
-        <div class="location events-location">
-          <img src="assets/img/events/event-ic-location.svg" width="12px" height="12px"
-               class="events-img-fourth"/>
-          item.location
-        </div>
-        <img class="img-fluid events-img-fluid" src="https://images.unsplash.com/photo-1531058020387-3be344556be6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" />
-
-        <div class="card-body">
-          <h4 class="card-title events-header-fifth">item.name</h4>
-          <p class="card-text events-header-fifth">
-            item.detail
-          </p>
-
-          <div class="location">
-            <img src="assets/img/events/event-ic-time.svg" width="12px" height="12px"
-                 class="events-img-location"/>
-            item.time
+        <div>
+          <div class="events-head-div">
+          <img class="events-top-back-img" src={topLeft}/>
+            <div class="events-top-right-div">
+            <img class="events-img-top" src={topRight}/>
+            </div>
+            <div class="events-top-left-div">
+            <p class="events-para-title"> Ready to join the movement?</p>
+            <p class="events-para-description">Check out what's happening soon in the Campus.</p>
+            <div class="events-search-div">
+            <div class="events-search-left-div">
+            <img class="events-search-img" src={search}/>
+            <hr/>
+            </div>
+            <div class="events-search-right-div">
+            <input type="text" placeholder="Search Projects" class="events-search-text"/>
+            <hr/>
+            </div>
           </div>
-
+          </div>
+          </div>
+          <div class="events-hackslash-div">
+          <div class="events-hackslash-text-section">
+            <div class="events-upcoming-div"><p class="events-hackslash-para-upcoming">Upcoming</p></div>
+            <div class="events-past-div"><p class="events-hackslash-para-past">Past</p></div>
+          </div>
+          <div class="events-hackslash-underline-section">
+            <hr class="events-hackslash-line"/>
+          </div>
+          </div>
+          <div class="row events-list-div">
+          {/* <img class="events-left-side-vec" src={sideVector}/> */}
+          {/* <img class="events-left-side-under-vec" src={sideUnderVec}/> */}
+          {/* <img class="events-right-side-vec" src={rightVector}/> */}
+          <div class="mb-3 events-event-detail">
+            <div class="events-image-div">
+              <img class="events-image" src="https://in.bookmyshow.com/entertainment-news/wp-content/uploads/2018/01/Events-In-Chennai.jpg"/>
+            </div>
+            <div class="events-text-section">
+              <div class="events-heading-text-div">
+                <p class="events-heading-text">Kondoa Community Network School of Internet Governance</p>
+              </div>
+              <div class="events-description-text-div">
+                <p class="events-description-text">At aliquet mattis aliquam posuere. Congue ante ac ultrices nunc.</p>
+              </div>
+            </div>
+            <div class="events-datetime-div">
+              <div class="events-time-div">
+                <div class="events-time-image-div">
+                  <img class="events-timedate-image" src={date}/>
+                </div>
+                <div class="events-time-text-div"><p class="events-time-text">05-04-2020</p></div>
+              </div>
+              <div class="events-date-div">
+                <div class="events-date-image-div">
+                  <img class="events-timedate-image" src={time}/>
+                </div>
+                <div class="events-date-text-div"><p class="events-date-text">10:00AM-12:00PM</p></div>
+              </div>
+            </div>
+          </div>
+          <div class="mb-3 events-event-detail">
+            <div class="events-image-div">
+              <img class="events-image" src="https://in.bookmyshow.com/entertainment-news/wp-content/uploads/2018/01/Events-In-Chennai.jpg"/>
+            </div>
+            <div class="events-text-section">
+              <div class="events-heading-text-div">
+                <p class="events-heading-text">Kondoa Community Network School of Internet Governance</p>
+              </div>
+              <div class="events-description-text-div">
+                <p class="events-description-text">At aliquet mattis aliquam posuere. Congue ante ac ultrices nunc.</p>
+              </div>
+            </div>
+            <div class="events-datetime-div">
+              <div class="events-time-div">
+                <div class="events-time-image-div">
+                  <img class="events-timedate-image" src={date}/>
+                </div>
+                <div class="events-time-text-div"><p class="events-time-text">05-04-2020</p></div>
+              </div>
+              <div class="events-date-div">
+                <div class="events-date-image-div">
+                  <img class="events-timedate-image" src={time}/>
+                </div>
+                <div class="events-date-text-div"><p class="events-date-text">10:00AM-12:00PM</p></div>
+              </div>
+            </div>
+          </div>
+          <div class="mb-3 events-event-detail">
+            <div class="events-image-div">
+              <img class="events-image" src="https://in.bookmyshow.com/entertainment-news/wp-content/uploads/2018/01/Events-In-Chennai.jpg"/>
+            </div>
+            <div class="events-text-section">
+              <div class="events-heading-text-div">
+                <p class="events-heading-text">Kondoa Community Network School of Internet Governance</p>
+              </div>
+              <div class="events-description-text-div">
+                <p class="events-description-text">At aliquet mattis aliquam posuere. Congue ante ac ultrices nunc.</p>
+              </div>
+            </div>
+            <div class="events-datetime-div">
+              <div class="events-time-div">
+                <div class="events-time-image-div">
+                  <img class="events-timedate-image" src={date}/>
+                </div>
+                <div class="events-time-text-div"><p class="events-time-text">05-04-2020</p></div>
+              </div>
+              <div class="events-date-div">
+                <div class="events-date-image-div">
+                  <img class="events-timedate-image" src={time}/>
+                </div>
+                <div class="events-date-text-div"><p class="events-date-text">10:00AM-12:00PM</p></div>
+              </div>
+            </div>
+          </div>
+          <div class="mb-3 events-event-detail">
+            <div class="events-image-div">
+              <img class="events-image" src="https://in.bookmyshow.com/entertainment-news/wp-content/uploads/2018/01/Events-In-Chennai.jpg"/>
+            </div>
+            <div class="events-text-section">
+              <div class="events-heading-text-div">
+                <p class="events-heading-text">Kondoa Community Network School of Internet Governance</p>
+              </div>
+              <div class="events-description-text-div">
+                <p class="events-description-text">At aliquet mattis aliquam posuere. Congue ante ac ultrices nunc.</p>
+              </div>
+            </div>
+            <div class="events-datetime-div">
+              <div class="events-time-div">
+                <div class="events-time-image-div">
+                  <img class="events-timedate-image" src={date}/>
+                </div>
+                <div class="events-time-text-div"><p class="events-time-text">05-04-2020</p></div>
+              </div>
+              <div class="events-date-div">
+                <div class="events-date-image-div">
+                  <img class="events-timedate-image" src={time}/>
+                </div>
+                <div class="events-date-text-div"><p class="events-date-text">10:00AM-12:00PM</p></div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </div>
-</body>
+        </div>
 );
 };
         export default Events;
