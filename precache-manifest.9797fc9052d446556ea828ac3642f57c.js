5f5980adcd2e532a4160e6c9401f983d89def231
krharsh17 <krharsh17@users.noreply.github.com>
2020-05-05 21:02:46 +0000

    deploy: bd2bc6c96bc2502d0131a217231e6e9f5c00e072

---
+++ precache-manifest.9797fc9052d446556ea828ac3642f57c.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3273c2fc07fbab0d375b98efaab185a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2d8dd45873718c3d7c9",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d4f013af7032bf9fc2b6",
+    "url": "/static/css/main.99107d53.chunk.css"
+  },
+  {
+    "revision": "e2d8dd45873718c3d7c9",
+    "url": "/static/js/2.7f1b3405.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/static/js/2.7f1b3405.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4f013af7032bf9fc2b6",
+    "url": "/static/js/main.35f4be49.chunk.js"
+  },
+  {
+    "revision": "86f9dae0d5cff1685af0",
+    "url": "/static/js/runtime-main.9ec9233a.js"
+  },
+  {
+    "revision": "45e3bacab7c57da68f3f91a31b6a2ddf",
+    "url": "/static/media/GTWalsheimProRegular.45e3baca.ttf"
+  },
+  {
+    "revision": "982dd6eedc04628e40f7fbc6c79f7d05",
+    "url": "/static/media/about-bg-1.982dd6ee.svg"
+  },
+  {
+    "revision": "18cf487df21bef27f04351c9307b84aa",
+    "url": "/static/media/about-bg-2.18cf487d.svg"
+  },
+  {
+    "revision": "a8f8fcb90f017f103ff18bdc061a4fff",
+    "url": "/static/media/bottom-left.a8f8fcb9.svg"
+  },
+  {
+    "revision": "7ef13a047c85e221777df2933b39d6ed",
+    "url": "/static/media/bottom-right.7ef13a04.svg"
+  },
+  {
+    "revision": "9015246bf4969e8dd66f79448500e3df",
+    "url": "/static/media/brandon_grotesque.9015246b.otf"
+  },
+  {
+    "revision": "9188030db42306aea0bf6d7ddb57c365",
+    "url": "/static/media/certi-bg.9188030d.svg"
+  },
+  {
+    "revision": "165a5451866569fa81cbfad0be6faf8c",
+    "url": "/static/media/coderdojo.165a5451.png"
+  },
+  {
+    "revision": "bb8e6284970bcb367dbaa6fefb824b90",
+    "url": "/static/media/contribution.bb8e6284.svg"
+  },
+  {
+    "revision": "7e1d87156b3fd995872ac6dcbe7ed09f",
+    "url": "/static/media/designers-section.7e1d8715.svg"
+  },
+  {
+    "revision": "c49a9b026103ffa5745efe73325e9741",
+    "url": "/static/media/developers-section.c49a9b02.svg"
+  },
+  {
+    "revision": "7a26be0c3c975e0ed9604e174af6c0bb",
+    "url": "/static/media/fake_serif.7a26be0c.ttf"
+  },
+  {
+    "revision": "e3d070ef7b2d9b1d279e70972842b8d3",
+    "url": "/static/media/githubIcon.e3d070ef.svg"
+  },
+  {
+    "revision": "d2c8d0aae9c3557c69aa398705f2924b",
+    "url": "/static/media/leads-section.d2c8d0aa.svg"
+  },
+  {
+    "revision": "cddb83df4e8b3c42e879c3f262361612",
+    "url": "/static/media/linkedinIcon.cddb83df.svg"
+  },
+  {
+    "revision": "148104a3e24eff66f92675f93d0c4a94",
+    "url": "/static/media/lower-left.148104a3.svg"
+  },
+  {
+    "revision": "8e8e9c65f21a5c0ce082394cef610e48",
+    "url": "/static/media/lower-right.8e8e9c65.svg"
+  },
+  {
+    "revision": "eb8954f8485262c5de1f78b77fc574ca",
+    "url": "/static/media/middle-left.eb8954f8.svg"
+  },
+  {
+    "revision": "f7e8b400e4bda9577a0079d8a8a9e971",
+    "url": "/static/media/middle-right.f7e8b400.svg"
+  },
+  {
+    "revision": "f6bb9d6b12238ad6111d2e18ff023e23",
+    "url": "/static/media/slack-logo.f6bb9d6b.svg"
+  },
+  {
+    "revision": "b2c988b327b66fe824c0873e7e52f098",
+    "url": "/static/media/top-left.b2c988b3.svg"
+  },
+  {
+    "revision": "fb875c7d2e68dba2c3c6ca985ef7e7dd",
+    "url": "/static/media/top-right.fb875c7d.svg"
+  },
+  {
+    "revision": "5d1e950e718e259c4fdca8f4bd7e823c",
+    "url": "/static/media/whatsapp-logo.5d1e950e.svg"
+  }
+]);
