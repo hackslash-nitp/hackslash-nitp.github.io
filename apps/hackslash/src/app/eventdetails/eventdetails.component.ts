2442cc95371b550c9f426c5d6a3d2145f10d482f
archana3001 <56291695+archana3001@users.noreply.github.com>
2021-05-24 22:36:39 +0530

    Update eventdetails.component.ts

--- apps/hackslash/src/app/eventdetails/eventdetails.component.ts
+++ apps/hackslash/src/app/eventdetails/eventdetails.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { filter } from 'rxjs/operators';
-import { FormControl } from '@angular/forms';
-import { combineLatest, Observable, of } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'hackslash-eventdetails',
@@ -12,22 +8,14 @@ import { map, startWith } from 'rxjs/operators';
 })
 export class EventdetailsComponent implements OnInit {
 
-   ev: Observable<any>;
-  ev$:Observable<any>;
-  filter:FormControl;
-  filter$:Observable<string>;
-
+   ev: any;
   constructor(private http: HttpClient) {
-    this.ev = this.http.get<any>(
-      'https://www.eventbrite.com/o/hackslash-club-33466187871'//api of event brite
-    );
-      this.filter = new FormControl('');
-      this.filter$ = this.filter.valueChanges.pipe(startWith(''));
-    this.ev$ = combineLatest(this.ev, this.filter$).pipe(
-      map(([states, filterString]) => states.filter(state => state.name.toLowerCase().indexOf(filterString.toLowerCase()) !== -1))
-    );
+    this.http.get<any>(
+      'https://www.eventbriteapi.com/v3/organizations/544604903183/events/?token=QCITQBPHYXMKFWP3FXXP'
+    ).subscribe(res=>{
+       this.ev=res.events;
+        });
   }
-
   ngOnInit(): void {}
 
 }
