80951c4cc15dfd8e230aba1a590b814edc46b3fd
archana3001 <56291695+archana3001@users.noreply.github.com>
2021-05-25 09:43:32 +0530

    Update eventdetails.component.ts

--- apps/hackslash/src/app/eventdetails/eventdetails.component.ts
+++ apps/hackslash/src/app/eventdetails/eventdetails.component.ts
@@ -11,6 +11,7 @@ export class EventdetailsComponent implements OnInit {
    ev: any;
   constructor(private http: HttpClient) {
     this.http.get<any>(
+      // eventbrite Api
       'https://www.eventbriteapi.com/v3/organizations/544604903183/events/?token=QCITQBPHYXMKFWP3FXXP'
     ).subscribe(res=>{
        this.ev=res.events;
