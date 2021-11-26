6ff05efaa97462a17627f473ce0cf246a5f0575d
its-me-Harsh-Anand <harshvivek.787@gmail.com>
2021-11-26 17:56:53 +0530

    home 2 updated

--- components/Home/Home2.js
+++ components/Home/Home2.js
@@ -6,14 +6,14 @@ import styled from 'styled-components'
 export default function Home2() {
   return (
       <MainDiv>
-            <div>
-                <div>
-                    <h1>We are making ideas better for everyone</h1>
+            <ImageAndBody>
+                <ParagraphDiv>
+                    <Heading>We are making ideas better for everyone</Heading>
                     <p>simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when </p>
                     <button>Learn More</button>
-                </div>               
+                </ParagraphDiv>               
               <Image src={marketing} alt="marketing-boy-logo" width={400} height={400} />
-            </div>
+            </ImageAndBody>
 
             <div>
               {/* <div><Image /></div>
@@ -29,4 +29,17 @@ export default function Home2() {
 const MainDiv = styled.div`
   background: url('/home/Homepage_2_background.svg') center center/cover;
   height: 100vh;
+`
+const ImageAndBody = styled.div`
+  height: 80%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`
+const ParagraphDiv = styled.div`
+  width: 50%;
+`
+const Heading = styled.h3`
+  width: 400px;
+  color: white;
 `
