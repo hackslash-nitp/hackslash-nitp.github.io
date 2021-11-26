a2cf6abd279ce6003b07c7fd5613f600b1b28c2b
Pulkit Kumar Agarwal <63954083+pka5667@users.noreply.github.com>
2021-11-26 16:30:52 +0530

    About page update

--- components/About/About2.js
+++ components/About/About2.js
@@ -1,7 +1,35 @@
+import styled from 'styled-components'
+import Image from 'next/image'
+import Card from './Card'
+
 export default function About2() {
   return (
-    <div>
-      
-    </div>
+    <Container>
+        <Text>
+          <Heading>Our Team</Heading>
+          <span>conquering the world of tech</span>
+        </Text>
+        <CardContainer>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </CardContainer>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  
+`
+const Heading = styled.span`
+
+`
+
+const Text = styled.div`
+  
+`
+
+const CardContainer = styled.div`
+
+`
