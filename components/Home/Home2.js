fd1e8cc7f9ae85a7a48f55996bfe2803e7acf122
Pulkit Kumar Agarwal <63954083+pka5667@users.noreply.github.com>
2021-11-27 10:54:47 +0530

    Home2

--- components/Home/Home2.js
+++ components/Home/Home2.js
@@ -2,63 +2,6 @@ import Image from 'next/image'
 import marketing from '@/assets/home/home2logo.svg'
 import styled from 'styled-components'
 
-// export default function Home2() {
-//   return (
-//       <MainDiv>
-//             <ImageAndBody>
-//                 <ParagraphDiv>
-//                     <Heading>We are making <Em>ideas</Em> better for everyone</Heading>
-//                     <Para>simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when </Para>
-//                     <Button>Learn More</Button>
-//                 </ParagraphDiv>               
-//               <Image src={marketing} alt="marketing-boy-logo" width={400} height={400} />
-//             </ImageAndBody>
-
-//             <div>
-//               {/* <div><Image /></div>
-//               <div><Image /></div>
-//               <div><Image /></div>
-//               <div><Image /></div> */}
-//             </div>
-
-//         </MainDiv>
-//   )
-// }
-
-// const MainDiv = styled.div`
-//   background: url('/home/Homepage_2_background.svg') center center/cover;
-//   height: 100vh;
-// `
-// const ImageAndBody = styled.div`
-//   height: 80%;
-//   display: flex;
-//   justify-content: space-around;
-//   align-items: center;
-// `
-// const ParagraphDiv = styled.div`
-//   width: 50%;
-// `
-// const Heading = styled.h2`
-//   width: 400px;
-//   color: white;
-// `
-// const Em = styled.em`
-// font-family: Poppins;
-// color:#00F0FF;
-// `
-// const Para = styled.p`
-// font-size:20px;
-// color: #FFFFFF
-// `
-// const Button= styled.button`
-// border-radius:20px;
-// color:#FFFBFB;
-// font-size:30px;
-// padding: 7px;
-// background: linear-gradient(90deg, #49DDAC 5.88%, #5DB5DC 89.82%);
-
-// `
-
 export default function Home2() {
   return (
     <Container>
