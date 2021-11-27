e1d42c6704871129ef57c6d576748e51f48f7c05
its-me-Harsh-Anand <harshvivek.787@gmail.com>
2021-11-28 00:13:25 +0530

    profile card completed...go to /card

---
+++ pages/card.js
@@ -0,0 +1,11 @@
+import Card from "@/components/About/Card"
+
+export default function card() {
+    return (
+        <div>
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    )
+}
