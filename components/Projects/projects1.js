0f29e1ddcd8e08bf66fbe30b7a51f636a3984b68
Pulkit Kumar Agarwal <63954083+pka5667@users.noreply.github.com>
2021-11-27 20:54:31 +0530

    Project js main page done

--- components/Projects/projects1.js
+++ components/Projects/projects1.js
@@ -3,184 +3,75 @@ import Card from './Card'
 export default function projects1() {
   return (
     <>
-      <BGrid>
-        <UGrid1>
+      <Container>
+        <FlexDiv>
           <Text>Our Project</Text>
-        </UGrid1>
-        <UGrid2>
-          <Input />
-        </UGrid2>
-        <CardGrid1>
-          <Card />
-        </CardGrid1>
-        <CardGrid2>
-          <Card />
-        </CardGrid2>
-      </BGrid>
-{/* page 2 */}
-      <BGrid1>
-        <CardGrid3>
-          <Card />
-        </CardGrid3>
-        <CardGrid4>
-          <Card />
-        </CardGrid4>
-        <CardGrid5>
-          <Card />
-        </CardGrid5>
-        <CardGrid6>
-          <Card />
-        </CardGrid6>
-      </BGrid1>
-{/* page 3 */}
-      <BGrid2>
-        <CardGrid7>
-          <Card />
-        </CardGrid7>
-        <CardGrid8>
-          <Card />
-        </CardGrid8>
-        <CardGrid9>
-          <Card />
-        </CardGrid9>
-        <CardGrid10>
-          <Card />
-        </CardGrid10>
-      </BGrid2>
+          <Input type="text" name="name" placeholder="" />
+        </FlexDiv>
+        
+        <CardContainer>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+        </CardContainer>
+        
+      </Container>
     </>
   )
 }
-var BGrid = styled.div`
-    background: url('/errorPage/ErrorPage.svg');
-    text-align:center;
-    height:1000px;
-    display: grid;
-    grid-template-rows: 20px,20px,60px,60px;
-    grid-template-columns:repeat(2,1fr);
-    row-gap:10px;
-    column-gap: 10px;
-`
-var BGrid1 = styled.div`
-    background: url('/errorPage/ErrorPage.svg');
-    text-align:center;
-    height:1000px;
-    display: grid;
-    grid-template-rows:80px,80px;
-    grid-template-columns:repeat(2,1fr);
-    row-gap:10px;
-    column-gap: 10px;
-`
-var BGrid2 = styled.div`
-    background: url('/errorPage/ErrorPage.svg');
-    text-align:center;
-    height:1000px;
-    display: grid;
-    grid-template-rows:80px,80px;
-    grid-template-columns:repeat(2,1fr);
-    row-gap:10px;
-    column-gap: 10px;
-`
-var UGrid1 = styled.div`
-// background-color:blue;
-font-size:30px;
- grid-column-start: 1;
- grid-column-end: 2;
- grid-row-start:2;
-grid-row-end: 3;
-`
-var UGrid2 = styled.div`
-// background-color:blue;
-grid-column-start: 2;
-grid-column-end: 3;
-grid-row-start:2;
-grid-row-end: 3;
-`
-var CardGrid1 = styled.div`
-// background-color:orange;
-grid-column-start: 1;
-grid-column-end: 2;
-grid-row-start:3;
-grid-row-end: 4;
-`
-var CardGrid2 = styled.div`
-// background-color:green;
-grid-column-start: 2;
-grid-column-end: 3;
-grid-row-start:3;
-grid-row-end: 4;
-`
-var CardGrid3 = styled.div`
-// background-color:orange;
-grid-column-start: 1;
-grid-column-end: 2;
-grid-row-start:1;
-grid-row-end: 2;
-`
-var CardGrid4 = styled.div`
-// background-color:green;
-grid-column-start: 2;
-grid-column-end: 3;
-grid-row-start:1;
-grid-row-end: 2;
-`
-var CardGrid5 = styled.div`
-// background-color:orange;
-grid-column-start: 1;
-grid-column-end: 2;
-grid-row-start:2;
-grid-row-end: 3;
-`
-var CardGrid6 = styled.div`
-// background-color:green;
-grid-column-start: 2;
-grid-column-end: 3;
-grid-row-start:2;
-grid-row-end: 3;
-`
-var CardGrid7 = styled.div`
-// background-color:orange;
-grid-column-start: 1;
-grid-column-end: 2;
-grid-row-start:1;
-grid-row-end: 2;
-`
-var CardGrid8 = styled.div`
-// background-color:green;
-grid-column-start: 2;
-grid-column-end: 3;
-grid-row-start:1;
-grid-row-end: 2;
-`
-var CardGrid9 = styled.div`
-// background-color:orange;
-grid-column-start: 1;
-grid-column-end: 2;
-grid-row-start:2;
-grid-row-end: 3;
-`
-var CardGrid10 = styled.div`
-// background-color:green;
-grid-column-start: 2;
-grid-column-end: 3;
-grid-row-start:2;
-grid-row-end: 3;
-`
-var Text = styled.h1`
-font-family: Poppins;
-font-style: normal;
-font-size: 96px;
-color:linear-gradient(94.77deg, #00FF9D 3.5%, #00E7FF 86.04%);
 
+var Container = styled.div`
+    background: url('/errorPage/ErrorPage.svg');
+    width: 100vw;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    padding: 10%;
 `
-// for input
 
-var Input = styled.input`
-  font-size:50px;
-  text-align:center;
-  width: 545px;
-  height: 78px;  
-  margin-top:100px;
-  border-radius:20px;
-  background: #6A6969;
-  border:4px solid linear-gradient(94.77deg, #00FF9D 3.5%, #00E7FF 86.04%);
+var FlexDiv = styled.div`
+  width: 60%;
+  display: flex;
+  justify-content: space-between;
+`
+
+const Text = styled.span`
+  background: linear-gradient(90deg, #00FF9D 3.5%, #00E7FF 86.04%);
+  background-clip: initial;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 3rem;
+  font-weight: 400;
+`
+
+const Input = styled.input`
+  width: 350px;
+  height: 60px;
+  border-radius: 35px;
+  color: white;
+  font-size: 1.2rem;
+  padding: 10px 15px;
+  margin:10px 0px;
+
+  font-weight: 300;
+  font-family: 'Poppins','sans-serif';
+  border: #69F8A2 solid 1.5px;
+  background-color: rgba(256, 256, 256, 0.3);
+  
+  &::placeholder{
+    color: whitesmoke;
+  }
+`
+
+
+
+// Card Container 
+
+var CardContainer = styled.div`
+  display: grid;
+  justify-content: space-between;
+  grid-template-columns: 50% 50%;
 `
