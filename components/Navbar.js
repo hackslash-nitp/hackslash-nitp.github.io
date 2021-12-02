28797f6f26d64e6dbd5c35478be733b5a005d0fa
Pulkit Kumar Agarwal <63954083+pka5667@users.noreply.github.com>
2021-12-02 12:49:40 +0530

    Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,33 +1,59 @@
-import styled from "styled-components"
+import styled, {css} from "styled-components"
 import Link from 'next/link'
 import Image from 'next/image'
 import Logo from '@/assets/hackslash_logo.svg'
-import {FaFacebook, FaInstagram, FaTwitter} from 'react-icons/fa'
+import {FaFacebook, FaInstagram, FaTwitter, FaBars} from 'react-icons/fa'
+import { Component } from "react"
 
-export default function Navbar() {
-    return (
-        <Nav>
-            <NavLeft> 
-                <Link href="/">
-                    <a>
-                        <Image src={Logo} alt="Hackslash Logo" height="20%"/> 
-                        <span>Hackslash</span>
-                    </a>
-                </Link>
-            </NavLeft>
-            <NavRight>
-                <Link href="/">Home</Link>
-                <Link href="/projects">Our Projects</Link>
-                <Link href="/about">About Us</Link>
-                <Link href="/contact">Contact Us</Link>
-                <div>
-                    <Link href="https://www.facebook.com/hackSlash.nitp/"><a><FaFacebook/></a></Link>
-                    <Link href="https://www.instagram.com/hackslash.nitp/"><a><FaInstagram/></a></Link>
-                    <Link href="https://twitter.com/hackslash_nitp/"><a><FaTwitter/></a></Link>
-                </div>
-            </NavRight>
-        </Nav>
-    )
+function myFunction(){
+    alert("ok")
+}
+
+export default class Navbar extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            nav_open: false,
+        }
+
+        this.toggle = this.toggle.bind(this)
+    
+    }
+
+    toggle() {
+        this.setState(() => ({'nav_open': !this.state.nav_open}))
+    }
+    
+    render() {
+        return (
+            <Nav>
+                <NavLeft> 
+                    <Link href="/">
+                        <a>
+                            <Image src={Logo} alt="Hackslash Logo" height="20%"/> 
+                            <span>Hackslash</span>
+                        </a>
+                    </Link>
+                </NavLeft>
+    
+                <NavRightContainer>
+                    <RightIcon onClick={this.toggle}><a><FaBars/></a></RightIcon>
+                    <NavRight isOpen={this.state.nav_open}>
+                        <Link href="/">Home</Link>
+                        <Link href="/projects">Our Projects</Link>
+                        <Link href="/about">About Us</Link>
+                        <Link href="/contact">Contact Us</Link>
+                        <div>
+                            <Link href="https://www.facebook.com/hackSlash.nitp/"><a><FaFacebook/></a></Link>
+                            <Link href="https://www.instagram.com/hackslash.nitp/"><a><FaInstagram/></a></Link>
+                            <Link href="https://twitter.com/hackslash_nitp/"><a><FaTwitter/></a></Link>
+                        </div>
+                    </NavRight>
+                </NavRightContainer>
+    
+            </Nav>
+        )
+    }
 }
 
 var Nav = styled.div`
@@ -43,9 +69,17 @@ var Nav = styled.div`
     z-index: 10;
     padding-left: 10%;
     padding-right: 10%;
+    padding-bottom: 10px;
     justify-content: space-between;
     background-color: rgba(0, 0, 0, 0.3);
-`
+
+    @media (max-width: 768px) {
+        max-height: max-content;
+        height: auto;
+        align-items: flex-start;
+        padding-top: 3%;
+    }
+    `
 
 var NavLeft = styled.div`
     width: 15%;
@@ -53,10 +87,21 @@ var NavLeft = styled.div`
     & > a{
         display: flex;
     }
+    `
+
+var NavRightContainer = styled.div`
+    width: 70%;
+
+    @media (max-width: 768px) {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        width: max-content;
+    }
 `
 
 var NavRight = styled.div`
-    width: 70%;
+    width: 100%;
     display: flex;
     justify-content: space-evenly;
 
@@ -67,5 +112,22 @@ var NavRight = styled.div`
         align-items: center;
         justify-content: space-evenly;
     }
-`
 
+    @media (max-width: 768px) {
+        flex-direction: column;
+        display: ${props => props.isOpen ? "flex" : "none"};
+
+        & > a{
+            margin-top: 2px;
+        }
+    }
+    `
+
+const RightIcon = styled.div`
+    display: none;
+    cursor: pointer;
+    
+    @media (max-width: 768px) {
+        display: initial;
+    }
+`
