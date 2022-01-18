fc0caa5de458a5b244bae12127d8522b39637829
its-me-Harsh-Anand <harshvivek.787@gmail.com>
2022-01-18 15:05:22 +0530

    community.mozila.org

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['res.cloudinary.com', 'drive.google.com', 'img.evbuc.com'],
+    domains: ['res.cloudinary.com', 'drive.google.com', 'img.evbuc.com', 'community.mozilla.org'],
   },
 }
